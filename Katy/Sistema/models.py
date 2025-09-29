ae51684ae13117377aa4a916d61fb66cb5bf553f
iitskaty <katyna.perez@inacapmail.cl>
2025-09-28 21:33:18 -0300

    pepe
    pepee

--- Katy/Sistema/models.py
+++ Katy/Sistema/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Visita(models.Model):
     nombre = models.CharField(max_length=100)
-    rut = models.TextField()
-    motivo_de_visita = models.DecimalField(max_digits=10, decimal_places=2)
+    rut = models.CharField(max_length=12, unique=True)
+    motivo_de_visita = models.CharField(max_length=256,default="")
     fecha_de_visita = models.DateField()
 
     def __str__(self):
